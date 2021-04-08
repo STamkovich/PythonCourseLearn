57d51d237389882aa8895bd267d38be9e3de3fe1
Sergey Tamkovich <tamkovich1072@gmail.com>
2021-04-08 23:24:44 +0300

    PythonDict.py

--- PythonDict.py
+++ PythonDict.py
@@ -13,3 +13,22 @@ for key, value in d.items():
 
 for key, value in d.items():
     print("КЛЮЧ: " + str(key) + " ,значения: " + str(value))
+
+# {'Alex': 25, 'Petr': 73}
+# {'Alex': 25, 'Petr': 73, 'Kate': 18}
+# 73
+# {'Alex': 25, 'Petr': 73, 'Kate': 24}
+# {'Alex': 25, 'Petr': 73, 'Kate': 24, 10: 20}
+# Alex
+# 25
+# Petr
+# 73
+# Kate
+# 24
+# 10
+# 20
+# КЛЮЧ: Alex ,значения: 25
+# КЛЮЧ: Petr ,значения: 73
+# КЛЮЧ: Kate ,значения: 24
+# КЛЮЧ: 10 ,значения: 20
+# {'first': [1, 2, 3], 'second': [10, 20], 'third': [15, 56, 70], 'fourth': [-50]}
